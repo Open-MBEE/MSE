0bcb6bd5f18f012a608e2a7c84c56508308c4965
Blake Regalia <blake.d.regalia@jpl.nasa.gov>
2020-09-01 11:32:58 -0700

    chore: fix file path

--- sublime-syntax/push-assets.sh
+++ sublime-syntax/push-assets.sh
@@ -2,8 +2,8 @@
 PACKAGE_VERSION="${TRAVIS_COMMIT_MESSAGE}"
 git checkout --orphan assets
 git reset --hard
-git rm --cached -r .
-git add sublime-syntax/build/sublime/MapleMBSE-MSE.sublime-package
+git rm --cached -r ../
+git add build/sublime/MapleMBSE-MSE.sublime-package
 git commit -m "v${PACKAGE_VERSION}"
 git remote add github-asset "https://${USERNAME}:${GITHUB_OAUTH_TOKEN}@github.com/Open-MBEE/MSE.git"
 git push -f github-asset assets
