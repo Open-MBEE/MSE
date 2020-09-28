900c8b9d65f2718bb08e42805a0d704ed4c91a03
Blake Regalia <blake.d.regalia@jpl.nasa.gov>
2020-09-28 09:31:34 -0700

    chore: push tag

--- sublime-syntax/push-assets.sh
+++ sublime-syntax/push-assets.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 PACKAGE_VERSION="${TRAVIS_COMMIT_MESSAGE}"
+GIT_TAG="v${PACKAGE_VERSION}-asset"
 git checkout --orphan assets
 git reset --hard
 git rm --cached -r ../
 git add build/sublime/MapleMBSE-MSE.sublime-package
 git commit -m "v${PACKAGE_VERSION}"
-git tag "v${PACKAGE_VERSION}-asset"
+git tag $GIT_TAG
 git remote add github-asset "https://${USERNAME}:${GITHUB_OAUTH_TOKEN}@github.com/Open-MBEE/MSE.git"
-git push -f github-asset assets
+git push -f github-asset assets && git push github-asset $GIT_TAG
